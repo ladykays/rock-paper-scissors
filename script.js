eaf9472f701284086027f7d9c4b274a9df2d9ade
Kerrion Siokwu <kerrionsiokwu@Kerrions-Mini.broadband>
2022-12-15 11:41:33 +0000

    Add index.html and script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Rules
+// Rock & Scissors = Rock Wins
+// Rock & Paper = Paper Wins
+// Paper & Scissors = Scissors Wins
+// Rock & Rock = Draw
+// Paper & Paper = Draw
+// Scissors & Scissors = Draw
+
+
+
